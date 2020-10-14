0fc3c64727a9c67ab481763b52839ee881f3f35c
meidelien <meidelien@gmail.com>
2020-10-14 11:56:04 +0200

    Update blenderScript.py
    PEP8 formatting

--- blenderScript.py
+++ blenderScript.py
@@ -2,90 +2,90 @@ import csv
 import bpy
 
 
-N_obj=10 #number of objects
-dim=3 #number of dimensions that the object s hould move in
-scale=10 # divides positions by scale such that fish are closer togheter
+N_obj=10 # Number of objects
+dim=3 # Number of dimensions that the objects hould move in
+scale=10 # Divides positions by scale such that fish are closer togheter
 
-#Set true if you want to import an .obj file that is suppose to be copied
+# Set true if you want to import an .obj file that is suppose to be copied
 importObject=False
-#Set true if you want to copy a object that is selected with cursour
+# Set true if you want to copy a object that is selected with cursour
 CopySelected=True
 
-#File path for import, the name of the imported object has to contain name_space
+# File path for import, the name of the imported object has to contain name_space
 obj_loc = '/home/jonas/Desktop/Salmon_texture.obj'
 
-#File path to csv that contains the locations of each object at each time frame.
-#each line contains a frame, first entry is a time stamp or somem other information.
-#then the next tree entries in that line are [x,y,z] positions 
+# File path to csv that contains the locations of each object at each time frame.
+# Each line contains a frame, first entry is a time stamp or some other information.
+# then the next tree entries in that line are [x,y,z] positions 
 csv_loc='/home/jonas/Desktop/dataForVisualization.CSV'  
 # csv_loc = r'C:\Users\meide\Documents\GitHub\iGEM2020_UiOslo_Norway/dataforVisualization.CSV' # Windows
 
-#The objects that are important or copied will only be found if their name 
-#contains this name space, this is not case sensetive
+# The objects that are important or copied will only be found if their name 
+# contains this name space, this is not case sensetive
 name_space="salmon"
 
 
-#Imports object 
+# Imports object 
 if importObject:
-    #Import N_obj objects and put them in scene
+    # Import N_obj objects and put them in scene
     for i in range(N_obj):
         new_obj = bpy.ops.import_scene.obj(filepath=obj_loc)
         
         
         
 if CopySelected:
-    #The scene that our object will be placed in
+    # The scene that our object will be placed in
     scn = bpy.context.scene.collection
     
-    #Object that we are going to copy, this is selected on the gui by the user
+    # Object that we are going to copy, this is selected on the gui by the user
     src_obj = bpy.context.active_object
     
-    #For N-1 we copy our object, -1 since we have one from before
+    # For N-1 we copy our object, -1 since we have one from before
     for i in range (N_obj-1):
         new_obj = src_obj.copy()
-        #change name such that the object can be found by it later
+        # Change name such that the object can be found by it later
         new_obj.name= "Salmon"
-        #copy object data
+        # Copy object data
         new_obj.data = src_obj.data.copy()
-        #clear animation data
+        # Clear animation data
         new_obj.animation_data_clear()
-        #set the object in our scene
+        # Set the object in our scene
         scn.objects.link(new_obj)
     
 
 
 
-#Array hold the objects that we want to animate
+# Array hold the objects that we want to animate
 our_objects=[]
-#Loop over all objects in scene
+# Loop over all objects in scene
 for ob in bpy.context.scene.objects:
-    #checks if object has the right name not case sensetiv
+    # Checks if object has the right name, not case sensitive
     if name_space.lower() in ob.name.lower():
-        #append to array
+        # Append to array
         our_objects.append(ob)
 
 
 
-#Frame number
+# Frame number
 frame_num=1
-#Open csv 
+# Open csv 
 with open(csv_loc, 'r', newline='') as csvfile:
-    #our file 
+    # our file 
     current_file = csv.reader(csvfile, delimiter=',')
     
-    #For each line (row) in current file
+    # For each line (row) in current file
     for line in current_file:
-        #f takes the time stamp first entry, then *pos contains all our positions 
+        # f takes the time stamp first entry, then *pos contains all our positions 
         f, *pos = line
         
-        #Convert positional values to floats
+        # Convert positional values to floats
         fpos = [float(p) for p in pos]
-        #Initialize array to hold coordinates
+        # Initialize array to hold coordinates
         coordinates=[]
         
-        #For N_obj append positional values for each agent to the coordinate array
+        # For N_obj append positional values for each agent to the coordinate array
         for i in range(N_obj):
-            #if dimension equals 2 then set z=0
+            # Ff dimension equals 2 then set z=0
             if dim==2:
                 temp_pos=[fpos[i-1]/scale,fpos[i]/scale,0]
                 coordinates.append(temp_pos)
@@ -94,15 +94,15 @@ with open(csv_loc, 'r', newline='') as csvfile:
                 temp_pos=[fpos[i-1]/scale,fpos[i]/scale,fpos[i+1]/scale]
                 coordinates.append(temp_pos)
         
-        #set frame
+        # Set frame
         bpy.context.scene.frame_set(frame_num)
-        #for each object and position 
+        # For each object and position 
         for ob, position in zip(our_objects, coordinates):
-            #assign new position
+            # Assign new position
             ob.location = position
-            #save frame
+            # Save frame
             ob.keyframe_insert(data_path="location", index=-1)
-        #count frame number
+        # Count frame number
         frame_num+=1
         
         
